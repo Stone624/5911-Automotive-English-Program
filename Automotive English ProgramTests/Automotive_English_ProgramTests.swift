f62571a27387f8a429d8b7fb8492abda1d7abe5f
Stone624 <Stonet.624@gmail.com>
2016-05-18 22:22:05 -0400

    Initial Commit

---
+++ Automotive English ProgramTests/Automotive_English_ProgramTests.swift
@@ -0,0 +1,36 @@
+//
+//  Automotive_English_ProgramTests.swift
+//  Automotive English ProgramTests
+//
+//  Created by Tyler Stone on 5/18/16.
+//  Copyright © 2016 Honda+OSU. All rights reserved.
+//
+
+import XCTest
+@testable import Automotive_English_Program
+
+class Automotive_English_ProgramTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
