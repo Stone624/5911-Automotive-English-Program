f628be3de5cc3836f411032af9d615e2dceb4c7d
Stone624 <Stonet.624@gmail.com>
2016-05-20 10:28:01 -0400

    pulled fresh from GH, copied video camera implementation, changed size of working psace, still testing git(hub)

--- Automotive English Program/ViewController.swift
+++ Automotive English Program/ViewController.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+import AVKit
+import MobileCoreServices
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var CaptureButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,25 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func CaptureButtonPressed(sender: UIButton) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera){
+            print("Camera Available.")
+            
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = .Camera
+            imagePicker.mediaTypes = [kUTTypeMovie as String]
+            imagePicker.allowsEditing = false
+            
+            imagePicker.showsCameraControls = true
+            self.presentViewController(imagePicker, animated: true, completion: nil)
+            
+        } else{
+            print("Camera Unavailable.")
+        }
+    }
+    
 
 }
 
