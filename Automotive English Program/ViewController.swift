540c9066fc30339995a371cc83326f570acb4fa7
Stone624 <Stonet.624@gmail.com>
2016-05-21 19:32:14 -0400

    Added FTP functionality (end product fails), temporarily a password field for secure connections, and some comments

--- Automotive English Program/ViewController.swift
+++ Automotive English Program/ViewController.swift
@@ -12,9 +12,21 @@ import MobileCoreServices
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    var password = ""
     let imagePicker: UIImagePickerController! = UIImagePickerController()
     let saveFileName = "outputVideo.mov"
     @IBOutlet weak var CaptureButton: UIButton!
+    @IBOutlet weak var PasswordField: UITextField!
+    
+    
+    @IBAction func PasswordEntered(sender: AnyObject) {
+        password = PasswordField.text!
+    }
+    func textFieldShouldReturn(textField: UITextField) -> Bool // called when 'return' key pressed. return false to ignore.
+    {
+        textField.resignFirstResponder()
+        return true
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +38,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         // Dispose of any resources that can be recreated.
     }
     
+    //Code that runs when the Capture Button is pressed. Should alert if the camera is unavailable, or open the camera, preferrebly in the front, if the camera is available. (2/3)
     @IBAction func CaptureButtonPressed(sender: UIButton) {
         if UIImagePickerController.isSourceTypeAvailable(/*UIImagePickerControllerSourceType*/.Camera){
             print("Camera Available.")
@@ -43,11 +56,11 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             self.presentViewController(imagePicker, animated: true, completion: nil)
             
         } else{
-            print("Camera Unavailable.")
+            print("ERROR: Camera Unavailable.")
         }
     }
     
-    
+    //Code that runs when the "use video" button is pressed after taking a video. Should get the temporary file URL of the video, and send it to a server for teachers to be able to view.
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
         print("Video Recording Finished. Got a Video!!!")
         if let pickedVideo:NSURL = (info[UIImagePickerControllerMediaURL] as? NSURL) {
@@ -63,10 +76,56 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
                 NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)
             let documentsDirectory: AnyObject = paths[0]
             let dataPath = documentsDirectory.stringByAppendingPathComponent(saveFileName)
-            videoData?.writeToFile(dataPath, atomically: false)
+            videoData?.writeToFile(dataPath, atomically: false) // << Could replace
             
             self.dismissViewControllerAnimated(true, completion: nil)
             
+            //Send video (via url) to FTP server
+            let username = "" // <<MUST BE HARD CODED
+            //password = "123"
+            let ip = "" // <<MUST BE HARD CODED
+            let fileName = "/Users/TylerStone/FtpFiles/5911AEP\(Int(NSDate().timeIntervalSince1970)).mov"
+            let FTPString = NSURL(string: "ftp://\(username):\(password)@\(ip):21/\(fileName)")
+            let FTPStream = CFWriteStreamCreateWithFTPURL(nil,FTPString!).takeUnretainedValue()
+            let cfstatus    = CFWriteStreamOpen(FTPStream)
+            // connection fail
+            print("VALUE OF FTPSTRING: \(FTPString)")
+            print("VALUE OF CFSTATUS: \(cfstatus)")
+            if cfstatus == false {
+                print("ERROR: Failed to connect to FTP server.")
+            }else{
+                var totalBytesWritten = 0
+                var bytesWritten = 0
+                var bytesLeft = (videoData?.length)!
+                let fileLength = (videoData?.length)!
+                
+                print("SENDING \(fileLength) BYTES:")
+                repeat{
+                    bytesWritten = CFWriteStreamWrite(FTPStream, dataPath, bytesLeft)
+                    print("BytesWritten: \(bytesWritten)")
+                    totalBytesWritten += bytesWritten
+                    if (bytesWritten < fileLength) {
+                        bytesLeft = fileLength - totalBytesWritten
+                    }else{
+                        bytesLeft = 0
+                    }
+                    print("Bytes Left: \(bytesLeft)")
+                    
+                    
+                }while((totalBytesWritten < fileLength && bytesWritten >= 0) /*|| (bytesLeft != 0)*/)
+                
+                
+                if totalBytesWritten == fileLength{
+                    print("Files Successfully transferred!")
+                }else{
+                    print("ERROR: File did not transfer successfully (bytes written != video length)")
+                }
+                print("Closing Stream...")
+                CFWriteStreamClose(FTPStream)
+            }
+            
+        } else {
+            print("ERROR: Failed to get video URL")
         }
         
         /*imagePicker.dismissViewControllerAnimated(true, completion: {
