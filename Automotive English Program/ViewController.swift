47d3ad92dde5926c500af58ec7fe339fc29efe51
Stone624 <Stonet.624@gmail.com>
2016-05-21 15:08:11 -0400

    improved video capture, saves locally and gets URL

--- Automotive English Program/ViewController.swift
+++ Automotive English Program/ViewController.swift
@@ -12,6 +12,8 @@ import MobileCoreServices
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    let imagePicker: UIImagePickerController! = UIImagePickerController()
+    let saveFileName = "outputVideo.mov"
     @IBOutlet weak var CaptureButton: UIButton!
     
     override func viewDidLoad() {
@@ -25,12 +27,15 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     @IBAction func CaptureButtonPressed(sender: UIButton) {
-        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera){
+        if UIImagePickerController.isSourceTypeAvailable(/*UIImagePickerControllerSourceType*/.Camera){
             print("Camera Available.")
-            
-            let imagePicker = UIImagePickerController()
-            imagePicker.delegate = self
             imagePicker.sourceType = .Camera
+            if UIImagePickerController.availableCaptureModesForCameraDevice(.Front) != nil{
+                print("Front camera available")
+                imagePicker.cameraDevice = UIImagePickerControllerCameraDevice.Front
+            }
+            
+            imagePicker.delegate = self
             imagePicker.mediaTypes = [kUTTypeMovie as String]
             imagePicker.allowsEditing = false
             
@@ -42,6 +47,33 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         }
     }
     
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        print("Video Recording Finished. Got a Video!!!")
+        if let pickedVideo:NSURL = (info[UIImagePickerControllerMediaURL] as? NSURL) {
+            print("Got a URL! It is: \(pickedVideo)")
+            // Save video to the main photo album
+            //let selectorToCall = Selector("videoWasSavedSuccessfully:didFinishSavingWithError:context:")
+            //UISaveVideoAtPathToSavedPhotosAlbum(pickedVideo.relativePath!, self, selectorToCall, nil)
+            
+            
+            // Save the video to the app directory so we can play it later
+            let videoData = NSData(contentsOfURL: pickedVideo)
+            let paths = NSSearchPathForDirectoriesInDomains(
+                NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)
+            let documentsDirectory: AnyObject = paths[0]
+            let dataPath = documentsDirectory.stringByAppendingPathComponent(saveFileName)
+            videoData?.writeToFile(dataPath, atomically: false)
+            
+            self.dismissViewControllerAnimated(true, completion: nil)
+            
+        }
+        
+        /*imagePicker.dismissViewControllerAnimated(true, completion: {
+            /* Anything you want to happen when the user saves an video*/
+            print("DISMISSED CONTROLLER.")
+        })*/
+    }
 
 }
 
